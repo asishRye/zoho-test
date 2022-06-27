8af7d91cf1f8fd093ff191c9ba9f98b66b775e8b
Asish Binu <asish@accubits.com>
2022-06-27 17:21:14 +0530

    :P

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, status
+import logging
+
 
 app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 class Zoho(BaseModel):
     data:str
